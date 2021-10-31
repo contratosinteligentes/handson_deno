78f4f80eac603a05e2165f4685a0d73154bc10f4
Fabiano Nascimento <seasinformatica@gmail.com>
2021-10-31 19:55:33 +0000

    Geracao de hash com biblioteca padrao do Deno

---
+++ 000.hash/handsOnHash.ts
@@ -0,0 +1,14 @@
+import { createHash } from "https://deno.land/std@0.110.0/hash/mod.ts";
+
+//KECCAK256
+const hashFrase = createHash("keccak256");
+hashFrase.update("Contratos Inteligentes: Hands on do episódio sobre hash.");
+console.log("hash da frase: ",hashFrase.toString());
+
+const hashPonto = createHash("keccak256");
+hashPonto.update(".");
+console.log("hash do ponto: ",hashPonto.toString());
+
+const hashmd5 = createHash("md5");
+hashmd5.update("Contratos Inteligentes: Hands on do episódio sobre hash.");
+console.log("hash com md5: ", hashmd5.toString());
